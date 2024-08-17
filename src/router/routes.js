09a9f370c9de13a3d3aefc9bcf90f77783562f52
JQuerel <bilmokooo@gmail.com>
2024-08-17 19:08:56 +0800

    route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,16 @@
 
+
+
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/CountryPage.vue') }
-    ]
+    component:  () => import('pages/CountryPage.vue'),
+    
   },
 
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/CountryPage.vue')
-  }
+ 
 ]
 
 export default routes
